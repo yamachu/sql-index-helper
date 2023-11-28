b23e7b65ee9947c93f06a2fb0d904286ceffb41c
yamachu <yamachu.dev@gmail.com>
2023-11-28 20:55:41 +0900

    add vite build config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/** @type {import('vite').UserConfig} */
+export default {
+  build: {
+    lib: {
+      entry: "src/transforms/index.ts",
+      fileName: "index",
+      formats: ["cjs"],
+    },
+    rollupOptions: {
+      external: ["jscodeshift"],
+    },
+  },
+};
